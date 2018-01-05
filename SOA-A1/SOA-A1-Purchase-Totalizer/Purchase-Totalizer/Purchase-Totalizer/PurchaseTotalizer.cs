df7d20953644cc599a93369c9262171c459994a6
lmorrison8857 <Lauchlin.A.B.Morrison@gmail.com>
2018-01-05 11:22:52 -0500

    Add solution for purchase totalizer.

---
+++ SOA-A1/SOA-A1-Purchase-Totalizer/Purchase-Totalizer/Purchase-Totalizer/PurchaseTotalizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchase_Totalizer
+{
+    static class PurchaseTotalizer
+    {
+        public static ReturnStruct Calculate(string locationCode, double value)
+        {
+            double pst = 0;
+            double hst = 0;
+            double gst = 0;
+            double total = 0;
+
+
+            return new ReturnStruct(value, pst, hst, gst, total);
+        }
+
+        public static bool ValidateLocationCode(string locationCode)
+        {
+
+
+            return false;
+        }
+
+    }
+}
---
+++ SOA-A1/SOA-A1-Purchase-Totalizer/Purchase-Totalizer/Purchase-Totalizer/ReturnStruct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purchase_Totalizer
+{
+    class ReturnStruct
+    {
+        public double Sub_total_amount;
+        public double PST_amount;
+        public double HST_amount;
+        public double GST_amount;
+        public double Total_purchase_amount;
+
+        public ReturnStruct(double sub, double pst, double hst, double gst, double total)
+        {
+            Sub_total_amount = sub;
+            PST_amount = pst;
+            HST_amount = hst;
+            GST_amount = gst;
+            Total_purchase_amount = total;
+        }
+    }
+}
