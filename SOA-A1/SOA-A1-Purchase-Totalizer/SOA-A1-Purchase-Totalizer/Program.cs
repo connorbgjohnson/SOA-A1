6141d6be7f1f2f2fe9d450b5ad462cfb57dfe5c0
lmorrison8857 <Lauchlin.A.B.Morrison@gmail.com>
2018-01-05 14:31:44 -0500

    Re-added the thing. Please man, work!

---
+++ SOA-A1/SOA-A1-Purchase-Totalizer/SOA-A1-Purchase-Totalizer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_A1_Purchase_Totalizer
+{
+    class Program
+    {
+        const string TAG_NAME = "GIORP-TOTAL";
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SOA-A1/SOA-A1-Purchase-Totalizer/SOA-A1-Purchase-Totalizer/ReturnStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_A1_Purchase_Totalizer
+{
+    public struct ReturnStruct
+    {
+        public bool Valid;
+        public decimal Sub_total_amount;
+        public decimal PST_amount;
+        public decimal HST_amount;
+        public decimal GST_amount;
+        public decimal Total_purchase_amount;
+
+        public ReturnStruct(bool valid, decimal sub, decimal pst, decimal hst, decimal gst, decimal total)
+        {
+            Valid = valid;
+            Sub_total_amount = sub;
+            PST_amount = pst;
+            HST_amount = hst;
+            GST_amount = gst;
+            Total_purchase_amount = total;
+        }
+    }
+}
