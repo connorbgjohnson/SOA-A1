c82b9ab537c0e10147c3bfc44c668b4a49683df1
lmorrison8857 <Lauchlin.A.B.Morrison@gmail.com>
2018-01-06 22:52:29 -0500

    Commiting partially build config stuff. It can be done after communication is added.

--- SOA-A1/SOA-A1-Purchase-Totalizer/SOA-A1-Purchase-Totalizer/Program.cs
+++ SOA-A1/SOA-A1-Purchase-Totalizer/SOA-A1-Purchase-Totalizer/Program.cs
@@ -3,24 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SOA_A1_Purchase_Totalizer
 {
     class Program
     {
+        const string CONFIG_FILE_PATH = "purchase_totalizer.config";
         const string TAG_NAME = "GIORP-TOTAL";
+
+        //List all the variables needed by the application here. Moving things to config files can be done later.
+        //string fasdfasdf
 
         static void Main(string[] args)
         {
-            TaxBreakdown test = PurchaseTotalizer.Calculate("AB", 899.99m);
-            Console.WriteLine("IsValid: {0}\nSub Total: {1}\nPST: {2}\nHST: {3}\nGST: {4}\nTotal: {5}", 
-                test.Valid, 
-                test.Sub_total_amount, 
-                test.PST_amount, 
-                test.HST_amount, 
-                test.GST_amount, 
-                test.Total_purchase_amount);
-
+            if(!LoadConfig())
+            {
+                Console.WriteLine("Error loading config.");
+            }
+            else
+            {
+                TaxBreakdown test = PurchaseTotalizer.Calculate("AB", 899.99m);
+                Console.WriteLine("IsValid: {0}\nSub Total: {1}\nPST: {2}\nHST: {3}\nGST: {4}\nTotal: {5}",
+                    test.Valid,
+                    test.Sub_total_amount,
+                    test.PST_amount,
+                    test.HST_amount,
+                    test.GST_amount,
+                    test.Total_purchase_amount);
+            }
         }
+
+
+        static bool LoadConfig()
+        {
+            bool status = false;
+
+            if (File.Exists(CONFIG_FILE_PATH))
+            {
+                status = true;  //REMOVE MEMEMADFAIFHUIGHFAGFUGAUIGFGAS
+                using (FileStream fileStream = File.OpenRead(CONFIG_FILE_PATH))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    string content = "";
+
+                    while (fileStream.Read(b, 0, b.Length) > 0)
+                    {
+                        content += temp.GetString(b);
+                    }
+
+
+                }
+            }
+            else
+            {
+                CreateConfig();
+            }
+
+            return status;
+        }
+
+
+        static void ParseConfig(string configString)
+        {
+            string[] fileByLine = configString.Split('\n');
+            foreach (string line in fileByLine)
+            {
+                //regex
+            }
+        }
+
+        static void CreateConfig()
+        {
+            string configFileString = @"teamname=
+teamId=
+tagName=
+serviceName=
+securityLevel=
+numArgs=
+numResponses=
+description=
+registryIP=
+registryPort=
+";
+            File.WriteAllText(CONFIG_FILE_PATH, configFileString);
+            Console.WriteLine("Created config file as none existed.");
+        }
+
     }
 }
